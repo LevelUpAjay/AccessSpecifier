9e63330bc89955150235bcc06ae4ea05433062c8
LevelUpAjay <ajayrawatonlinesite1@gmail.com>
2024-03-30 22:49:15 +0530

    Added AccessSpecifier2 and solution

---
+++ AccessDemo2/Four.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessDemo2
+{
+    class Four
+    {
+        static void Main()
+        {
+            AccessDemo1.Program objProg = new AccessDemo1.Program();
+            objProg.MethodPublic();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AccessDemo2/Three.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessDemo2
+{
+    class Three : AccessDemo1.Program
+    {
+        static void Main(string[] args)
+        {
+            Three objProg = new Three();
+            objProg.MethodPublic();
+            objProg.MethodProtected();
+            objProg.MethodProtectedInternal();
+            Console.ReadLine();
+        }
+    }
+}
